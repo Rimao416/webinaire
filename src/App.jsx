018c82ca4483492f571d234717171179f0a77951
Rimao416 <kayumbaomari27@gmail.com>
2025-03-08 02:23:28 +0100

    03_Counter_app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter,setCounter]=useState(0);
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold text-white animate-bounce">
-        Bienvenue !
-      </h1>
-      <p className="mt-4 text-xl text-gray-200 text-center">
-        Nous sommes ravis de vous accueillir à notre formation.
-      </p>
-      <p className="mt-2 text-lg text-gray-200 text-center">
-        Prêt à commencer votre voyage ? 🚀
-      </p>
-      <button className="mt-8 px-6 py-3 bg-white text-purple-600 font-semibold rounded-lg shadow-lg hover:bg-purple-100 transition duration-300">
-        Commencer
-      </button>
+     <p className='text-4xl font-bold text-white'  >{counter}</p>
+     {
+      counter==0 && <p className='text-4xl font-bold text-white shadow-md p-2'>Vous avez atteint la limite</p>
+     }
+
+     <button className="bg-white text-black py-2 px-4 rounded cursor-pointer mt-4" onClick={() => setCounter(counter + 1)}>Augmenter</button>
+     <button className='bg-white text-black py-2 px-4 rounded cursor-pointer mt-4' onClick={() => setCounter(counter - 1)} disabled={counter==0}>Diminuer</button>
+
     </div>
   );
 }
